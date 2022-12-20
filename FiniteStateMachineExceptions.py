8dabd4d7dce60129561b19368ec8e30b454e4b85
tachank-a <gildo2015@yandex.ru>
2022-12-21 04:57:26 +0700

    add exceptions

---
+++ FiniteStateMachineExceptions.py
@@ -0,0 +1,14 @@
+class WrongModifierException(Exception):
+    pass
+
+
+class DuplicateVariableNamesException(Exception):
+    pass
+
+
+class InvalidExpressionException(Exception):
+    pass
+
+
+class FirstDigitInTheNameException(Exception):
+    pass
