a6721be0e872911d5462979f438e53915d3cb44d
tachank-a <gildo2015@yandex.ru>
2022-12-21 04:56:07 +0700

    moved the alphabet to another file

---
+++ Resources.py
@@ -0,0 +1,15 @@
+modifiers = {"int", "char", "double", "float", "long"}
+long = "long"
+short = "short"
+
+numbers = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'}
+alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+             'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+             'y', 'z', '_'}
+
+special_characters = {' ', '\n', '\t', '\r'}
+
+opening_bracket = '['
+closing_bracket = ']'
+comma = ','
+semicolon = ';'
